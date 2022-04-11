e3a3b2dc244d89f51b32cfbd5dbe573278024820
alban-okoby <albanokoby225@gmail.com>
2022-04-11 16:38:16 +0000

    Create thing.js
    Création et exportation d'un schéma de données

---
+++ backend/models/thing.js
@@ -0,0 +1,13 @@
+// Création de schéma de données
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+// Exportation du schéma
+module.exports = mongoose.model('Thing', thingSchema);
