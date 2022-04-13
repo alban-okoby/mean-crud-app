bc1a658960a77fb345b71d7840bbf84dfff540ca
alban-okoby <albanokoby225@gmail.com>
2022-04-13 01:37:35 +0000

    Create stuff.js
    On se créer un stuff pour nos contrôleurs

---
+++ backend/controllers/stuff.js
@@ -0,0 +1,27 @@
+
+
+const Thing = require('../models/thing');
+
+
+exports.createThing = (req, res, next) => {
+    const thing = new Thing({
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
+    });
+  thing.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
