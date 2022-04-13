1dfb63a6a226b0e2751e3b40d6f17328ff651bab
alban-okoby <albanokoby225@gmail.com>
2022-04-13 03:52:26 +0000

    Mise en place de route utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 
 // Connexion à mongoBD
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('api/auth', userRoutes);
   
 // Exportation de app
 module.exports = app;
